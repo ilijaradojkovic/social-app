ea5ab70c296b22d17d240aaa47d03b6cd04057b7
Nevena123-bit <nevenasavov8@gmail.com>
2022-07-12 00:55:51 +0200

    final commit

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,20 @@
+import { AUTH, LOGOUT } from "../constants/actionTypes";
+
+const authReducer = (state = { authData: null }, action) => {
+	switch (action.type) {
+		case AUTH:
+			localStorage.setItem("profile", JSON.stringify({ ...action?.data }));
+
+			// return { ...state, authData: action.data, loading: false, errors: null };
+			return { ...state, authData: action?.data };
+		case LOGOUT:
+			localStorage.clear();
+
+			return { ...state, authData: null };
+		// 	return { ...state, authData: null, loading: false, errors: null };
+		default:
+			return state;
+	}
+};
+
+export default authReducer;
